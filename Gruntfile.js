a23f6c59f041919e31318c8967a7adfa734ef4cd
nyatto <nyatto73@gmail.com>
2014-02-04 21:26:29 +0900

    Update to Bootstrap v3.1.0
    First attempt using Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Task configuration.
+    clean: {
+      dist: 'build'
+    },
+
+    jshint: {
+      // options: {
+      //   jshintrc: 'js/.jshintrc'
+      // },
+      grunt: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: 'js/*.js'
+      }
+    },
+
+    csslint: {
+      // options: {
+      //   csslintrc: 'less/.csslintrc'
+      // },
+      src: [
+        'build/css/html5reset.css',
+        'build/css/styles.css'
+      ]
+    },
+
+    less: {
+      development: {
+        files: {
+          'src/css/styles.css': './less/styles.less'
+        }
+      },
+      production: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          'build/css/styles.css': './less/styles.less'
+        }
+      }
+    },
+
+    csscomb: {
+      sort: {
+        options: {
+          config: 'less/.csscomb.json'
+        },
+        files: {
+          'build/css/html5reset.css': 'build/css/html5reset.css',
+          'build/css/styles.css': 'build/css/styles.css'
+        }
+      }
+    },
+
+    copy: {
+      fonts: {
+        expand: true,
+        src: 'fonts/*',
+        dest: 'build/'
+      },
+      files: {
+        expand: true,
+        cwd: 'src/',
+        src: ['**'],
+        dest: 'build/'
+      }
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          hostname: 'localhost',
+          base: 'build'
+        }
+      }
+    },
+
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile']
+      },
+      less: {
+        files: 'less/*.less',
+        tasks: 'less'
+      },
+      htmls: {
+        files: '**/*.html'
+      }
+    }
+  });
+
+
+  // These plugins provide necessary tasks.
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+
+  // Default task.
+  grunt.registerTask('default', ['clean', 'jshint', 'less', 'csscomb', 'copy', 'connect', 'watch']);
+  // Task for build.
+  grunt.registerTask('build', ['clean', 'copy', 'connect', 'watch']);
+
+};
